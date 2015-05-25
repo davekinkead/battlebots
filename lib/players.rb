a104a3b658e715ef8142af319f33a169521a44bf
elliotthilaire <elliott.hilaire@gmail.com>
2015-05-25 22:15:07 +1000

    add to players.rb

--- lib/players.rb
+++ lib/players.rb
@@ -3,6 +3,7 @@ require 'bots/the_closer'
 require 'bots/the_bully'
 require 'bots/the_chicken'
 require 'bots/the_thinker'
+require 'bots/death_roomba'
 
 module BattleBots
   module Players
@@ -11,7 +12,8 @@ module BattleBots
       [ Proxy.new(self, TheCloser), 
         Proxy.new(self, TheBully),
         Proxy.new(self, TheThinker),
-        Proxy.new(self, TheChicken)].shuffle
+        Proxy.new(self, TheChicken),
+        Proxy.new(self, DeathRoomba)].shuffle
     end
   end
 end
