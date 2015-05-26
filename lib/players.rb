2fe51828b6e0027224502e7244fbc925c6ee754c
Alex Ghiculescu <alexghiculescu@gmail.com>
2015-05-26 12:35:31 +1000

    serious contender

--- lib/players.rb
+++ lib/players.rb
@@ -3,6 +3,7 @@ require 'bots/the_closer'
 require 'bots/the_bully'
 require 'bots/the_chicken'
 require 'bots/the_thinker'
+require 'bots/spray_and_pray'
 
 module BattleBots
   module Players
@@ -11,6 +12,7 @@ module BattleBots
       [ Proxy.new(self, TheCloser), 
         Proxy.new(self, TheBully),
         Proxy.new(self, TheThinker),
+        Proxy.new(self, SprayAndPray),
         Proxy.new(self, TheChicken)].shuffle
     end
   end
